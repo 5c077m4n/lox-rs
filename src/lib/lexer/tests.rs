08408b8757365c05af96fbee9f97c61a966094de
Roee Shapira <roee.shapira@tensorleap.ai>
2022-10-29 00:36:30 +0300

    Add ignore to space chars

--- src/lib/lexer/tests.rs
+++ src/lib/lexer/tests.rs
@@ -4,7 +4,10 @@ use super::{
 	super::lexer::tokens::token_type::{Keyword, TokenType},
 	scanner::scan,
 };
-use crate::lib::lexer::tokens::token::Token;
+use crate::lib::lexer::tokens::{
+	token::Token,
+	token_type::{Literal, Operator},
+};
 
 #[test]
 fn sanity() -> Result<()> {
@@ -14,6 +17,22 @@ fn sanity() -> Result<()> {
 	assert_eq!(
 		input,
 		vec![Token::new(TokenType::Keyword(Keyword::Null), "", 0, 0)]
+	);
+	Ok(())
+}
+
+#[test]
+fn one_plus_one() -> Result<()> {
+	let input = b"1 + 1";
+	let input: Vec<Token> = scan(input).collect();
+
+	assert_eq!(
+		input,
+		vec![
+			Token::new(TokenType::Literal(Literal::Number(1.)), "", 0, 0),
+			Token::new(TokenType::Operator(Operator::Add), "", 0, 0),
+			Token::new(TokenType::Literal(Literal::Number(1.)), "", 0, 0),
+		]
 	);
 	Ok(())
 }
