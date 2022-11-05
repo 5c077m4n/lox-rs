00d558505f66e2f5cac5caf01e28c96319ce63ba
Roee Shapira <roee.shapira@tensorleap.ai>
2022-11-05 20:45:32 +0200

    Change Expr to be tuple enum from object enum

--- src/rlox/ast/visitor_fn.rs
+++ src/rlox/ast/visitor_fn.rs
@@ -2,17 +2,17 @@ use super::expr::Expr;
 
 pub fn parenthesize(expr: &Expr) -> String {
 	match expr {
-		Expr::Binary { left, op, right } => {
+		Expr::Binary(left, op, right) => {
 			let left_str = &parenthesize(left);
 			let right_str = &parenthesize(right);
 			format!("({} {} {})", left_str, op.to_str(), right_str)
 		}
-		Expr::Grouping { expr } => {
+		Expr::Grouping(expr) => {
 			let expr_str = &parenthesize(expr);
 			format!("(group {})", expr_str)
 		}
-		Expr::Literal { value } => value.to_string(),
-		Expr::Unary { op, right } => {
+		Expr::Literal(value) => value.to_string(),
+		Expr::Unary(op, right) => {
 			let right_str = &parenthesize(right);
 			format!("({} {})", op.to_str(), right_str)
 		}
