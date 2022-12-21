0e17202a1ae89b8ea014c01c1d8805323d9f2f35
Roee Shapira <roee.shapira@tensorleap.ai>
2022-12-21 10:39:50 +0200

    wip! Add custom fn decl

--- src/lox_rs/ast/callables/builtins.rs
+++ src/lox_rs/ast/callables/builtins.rs
@@ -1,13 +1,17 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+	sync::Mutex,
+	time::{SystemTime, UNIX_EPOCH},
+};
+
+use once_cell::sync::Lazy;
 
 use super::{super::expr::Literal, callable::Callable};
 
-pub const CLOCK: Callable = Callable {
-	arity: 0,
-	as_str: "<native fn>",
-	func: |_| {
+pub static CLOCK: Lazy<Mutex<Callable>> = Lazy::new(|| {
+	let func = Callable::new(0, "<native fn>".to_string(), |_| {
 		let seconds = SystemTime::now().duration_since(UNIX_EPOCH)?;
 		let seconds = seconds.as_secs_f64();
 		Ok(Literal::Number(seconds))
-	},
-};
+	});
+	Mutex::new(func)
+});
--- src/lox_rs/ast/callables/callable.rs
+++ src/lox_rs/ast/callables/callable.rs
@@ -2,25 +2,32 @@ use std::fmt;
 
 use anyhow::Result;
 
-use super::super::expr::Literal;
+use super::super::{super::ast::visitors::interp::Interperter, expr::Literal};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Callable {
-	pub(super) arity: usize,
-	pub(super) as_str: &'static str,
-	pub(super) func: fn(Vec<Literal>) -> Result<Literal>,
+	arity: usize,
+	as_string: String,
+	func: fn(Vec<Literal>) -> Result<Literal>,
 }
 
 impl Callable {
+	pub fn new(arity: usize, as_string: String, func: fn(Vec<Literal>) -> Result<Literal>) -> Self {
+		Self {
+			arity,
+			as_string,
+			func,
+		}
+	}
 	pub fn arity(&self) -> usize {
 		self.arity
 	}
-	pub fn call(&self, args: Vec<Literal>) -> Result<Literal> {
+	pub fn call(&self, _interp: &Interperter, args: Vec<Literal>) -> Result<Literal> {
 		(self.func)(args)
 	}
 }
 impl fmt::Display for Callable {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "{:?}", self.as_str)
+		write!(f, "{:?}", self.as_string)
 	}
 }
