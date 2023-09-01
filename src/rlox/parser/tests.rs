a7187829747a06ea486d9d4fee8c064cb18904bc
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:02:47 +0300

    Add parser tests

--- src/rlox/parser/tests.rs
+++ src/rlox/parser/tests.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use super::{
 	super::{
 		ast::expr::{Expr, Literal},
-		lexer::scanner::scan,
+		lexer::{scanner::scan, tokens::token_type::Operator},
 	},
 	Parser,
 };
@@ -21,6 +21,62 @@ fn sanity() -> Result<()> {
 		tree,
 		Expr::Literal {
 			value: Literal::Null
+		}
+	);
+
+	Ok(())
+}
+
+#[test]
+fn one_plus_one() -> Result<()> {
+	let input = b"1 + 1";
+	let input = scan(input);
+
+	let mut parser = Parser::new(input);
+	let (tree, errors) = parser.parse()?;
+
+	assert!(errors.is_empty());
+	assert_eq!(
+		tree,
+		Expr::Binary {
+			left: Box::new(Expr::Literal {
+				value: Literal::Number(1.)
+			}),
+			op: Operator::Add,
+			right: Box::new(Expr::Literal {
+				value: Literal::Number(1.)
+			}),
+		}
+	);
+
+	Ok(())
+}
+
+#[test]
+fn one_plus_one_mul_one() -> Result<()> {
+	let input = b"1 + 1 * 1";
+	let input = scan(input);
+
+	let mut parser = Parser::new(input);
+	let (tree, errors) = parser.parse()?;
+
+	assert!(errors.is_empty());
+	assert_eq!(
+		tree,
+		Expr::Binary {
+			left: Box::new(Expr::Literal {
+				value: Literal::Number(1.)
+			}),
+			op: Operator::Add,
+			right: Box::new(Expr::Binary {
+				left: Box::new(Expr::Literal {
+					value: Literal::Number(1.)
+				}),
+				op: Operator::Mul,
+				right: Box::new(Expr::Literal {
+					value: Literal::Number(1.)
+				})
+			}),
 		}
 	);
 
